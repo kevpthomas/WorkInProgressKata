b6ee3901096311a1fc2553a5e3d9ba364628b9f9
kevin.thomas <kevin.thomas@equinox.co.nz>
2018-12-05 16:29:46 +1300

    Wire up RunnerIoCConfig so that TinyIoC registers correctly in the Console application.

--- WipKata.Runner/Program.cs
+++ WipKata.Runner/Program.cs
@@ -5,7 +5,11 @@ namespace WipKata.Runner
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            RunnerIoCConfig.Instance.Register();
+ 
+            Console.Clear();
+
             Console.WriteLine("Hello World!");
         }
     }
---
+++ WipKata.Runner/RunnerIoCConfig.cs
@@ -0,0 +1,23 @@
+﻿using IoC;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+// ReSharper disable StringLiteralTypo
+
+namespace WipKata.Runner
+{
+    public class RunnerIoCConfig : IoCConfig
+    {
+        private readonly Lazy<Assembly[]> _iocAssemblies = new Lazy<Assembly[]>(() =>
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.Combine(path, "WipKata.Infrastructure.dll"));
+            return AppDomain.CurrentDomain.GetAssemblies();
+        });
+
+        protected override Assembly[] IocAssemblies => _iocAssemblies.Value;
+
+        public static RunnerIoCConfig Instance { get; } = new RunnerIoCConfig();
+    }
+}
